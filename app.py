cdc84d2d23f58d5fe9fa8513cd976b562ce3ec92
Rippouzza <rayen.ghileb9363@istic.ucar.tn>
2025-03-08 14:02:09 +0100

    cleaning

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS  
 from controllers.file_upload_controller import upload_file
-from controllers.auth_controller import authenticate
+
 from controllers.plant_controller import plant_bp  # Import plant routes
 from controllers.supplier_controller import supplier_bp  # Import supplier routes
 from controllers.machine_controller import machine_bp
@@ -20,9 +20,7 @@ def home():
 def upload_file_route():
     return upload_file()
 
-@app.route('/authenticate', methods=['POST'])
-def authenticate_route():
-    return authenticate()
+
 
 # Register blueprints
 app.register_blueprint(plant_bp, url_prefix='/api')
